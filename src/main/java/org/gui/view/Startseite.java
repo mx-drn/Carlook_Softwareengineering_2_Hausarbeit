05cab87ea90ac07655cc0febf722490639b50c00
mx-drn <max.dueren@googlemail.com>
2020-08-26 16:03:56 +0200

    Erster Entwurf der Startseite

--- src/main/java/org/gui/view/Startseite.java
+++ src/main/java/org/gui/view/Startseite.java
@@ -2,6 +2,9 @@ package org.gui.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 public class Startseite implements View {
 
@@ -10,6 +13,24 @@ public class Startseite implements View {
     }
 
     public void setUp () {
-        
+        HorizontalLayout horiMain = new HorizontalLayout();
+        VerticalLayout vertMain = new VerticalLayout();
+
+        VerticalLayout verticalMid = new VerticalLayout();
+        verticalMid.setWidth("62%"); //circa Goldener Schnitt
+
+        //E-Mail Adresse für den Login
+        TextField email = new TextField("E-Mail");
+        email.setValue("Ihre E-Mail Adresse");
+        verticalMid.addComponent(email);
+
+        //Passwort
+        TextField passwort = new TextField("Passwort");
+        passwort.setValue("Ihr Passwort");
+        verticalMid.addComponent(passwort);
+
+
+
+
     }
 }
