ca737c46d981d92b20bce9a2ddb28133f753cae2
mx-drn <max.dueren@googlemail.com>
2020-08-27 11:13:40 +0200

    Startseite verbessert

--- src/main/java/org/gui/components/Footer.java
+++ src/main/java/org/gui/components/Footer.java
@@ -1,12 +1,18 @@
 package org.gui.components;
 
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.VerticalLayout;
 
 public class Footer extends VerticalLayout {
 
     public Footer () {
-        this.addComponent(new FooterTop());
-        this.addComponent(new FooterCopyright());
+        FooterTop footerTop = new FooterTop();
+        this.addComponent(footerTop);
+        this.setComponentAlignment(footerTop, Alignment.MIDDLE_CENTER);
+
+        FooterCopyright footerCopyright = new FooterCopyright();
+        this.addComponent(footerCopyright);
+        this.setComponentAlignment(footerCopyright, Alignment.MIDDLE_CENTER);
     }
 
 }
--- src/main/java/org/gui/view/Startseite.java
+++ src/main/java/org/gui/view/Startseite.java
@@ -2,11 +2,13 @@ package org.gui.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.ui.*;
+import org.gui.components.Footer;
+import org.gui.components.TopPanel;
 
-public class Startseite implements View {
+public class Startseite extends VerticalLayout implements View {
+    private Label whitespace = new Label("&nbsp", ContentMode.HTML);
 
     public void enter (ViewChangeListener.ViewChangeEvent viewChangeEvent) {
         this.setUp();
@@ -14,23 +16,57 @@ public class Startseite implements View {
 
     public void setUp () {
         HorizontalLayout horiMain = new HorizontalLayout();
-        VerticalLayout vertMain = new VerticalLayout();
 
         VerticalLayout verticalMid = new VerticalLayout();
         verticalMid.setWidth("62%"); //circa Goldener Schnitt
 
         //E-Mail Adresse für den Login
         TextField email = new TextField("E-Mail");
-        email.setValue("Ihre E-Mail Adresse");
+        email.setDescription("Ihre E-Mail Adresse");
         verticalMid.addComponent(email);
 
         //Passwort
+        HorizontalLayout horizontalLayoutLogin = new HorizontalLayout();
         TextField passwort = new TextField("Passwort");
-        passwort.setValue("Ihr Passwort");
-        verticalMid.addComponent(passwort);
+        passwort.setDescription("Ihr Passwort");
+        horizontalLayoutLogin.addComponent(passwort);
 
+        Button anmelden = new Button("Anmelden");
+        horizontalLayoutLogin.addComponent(anmelden);
 
+        verticalMid.addComponent(horizontalLayoutLogin);
+        verticalMid.setMargin(true);
 
+        //Registrieren feld
+        VerticalLayout verticalMidRight = new VerticalLayout();
+
+        Label registriertfrage = new Label("Noch nicht registriert?");
+
+        Button registrieren = new Button("Registrieren");
+
+        registrieren.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                    //Verlinkung auf registrierungsseite navigateTo()
+            }
+        });
+
+        verticalMidRight.addComponent(registriertfrage);
+        verticalMidRight.addComponent(whitespace);
+        verticalMidRight.addComponent(whitespace);
+        verticalMidRight.addComponent(registrieren);
+
+        //Layouts zusammenfügen
+
+        horiMain.addComponent(verticalMid);
+        horiMain.addComponent(verticalMidRight);
+
+        addComponent(horiMain);
+        setComponentAlignment(horiMain, Alignment.MIDDLE_CENTER);
+
+        Footer footer = new Footer();
+        addComponent(footer);
+        setComponentAlignment(footer, Alignment.MIDDLE_CENTER);
 
     }
 }
